9aa016f2d95b7f572c8a3a40f49fd952b5b6e68e
marloustolk <marloustolk@gmail.com>
2021-01-03 19:58:14 +0100

    Day 5

--- src/main/java/advent/of/code/AdventOfCodeRunner.java
+++ src/main/java/advent/of/code/AdventOfCodeRunner.java
@@ -1,17 +1,15 @@
 package advent.of.code;
 
-import advent.of.code._2020.Day1_ExpenseFinder;
-import advent.of.code._2020.Day2_PasswordDebugger;
-import advent.of.code._2020.Day3_TreeCounter;
-import advent.of.code._2020.Day4_PassportValidator;
+import advent.of.code._2020.*;
 
 public class AdventOfCodeRunner {
 
     public static void main(String[] args) {
-        new Day1_ExpenseFinder();
-        new Day2_PasswordDebugger();
-        new Day3_TreeCounter();
-        new Day4_PassportValidator();
+//        new Day1_ExpenseFinder();
+//        new Day2_PasswordDebugger();
+//        new Day3_TreeCounter();
+//        new Day4_PassportValidator();
+        new Day5_SeatDecoder();
     }
 
 }
--- src/main/java/advent/of/code/_2020/Day5_SeatDecoder.java
+++ src/main/java/advent/of/code/_2020/Day5_SeatDecoder.java
@@ -1,4 +1,70 @@
 package advent.of.code._2020;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
 public class Day5_SeatDecoder {
+
+    public Day5_SeatDecoder(){
+        first();
+        second();
+    }
+
+    static void first() {
+        List<BoardingPass> boardingPasses = getBoardingPasses().stream().map(BoardingPass::new).collect(toList());
+        BoardingPass boardingPass = boardingPasses.stream()
+                .max(Comparator.comparingInt(pass -> pass.seatId))
+                .orElseThrow(IllegalArgumentException::new);
+        System.out.println(String.format("Day 5.1. %s, answer = %s is highest seatId", boardingPass, boardingPass.seatId));
+    }
+
+    static void second() {
+        List<Integer> boardingPasses = getBoardingPasses().stream().map(string -> new BoardingPass(string).seatId).collect(toList());
+        int myPass = IntStream.range(0, 838)
+                .filter(i -> !boardingPasses.contains(i))
+                .filter(i -> boardingPasses.contains(i + 1) && boardingPasses.contains(i - 1))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+        System.out.println(String.format("Day 5.2. [number is not in list, but +1 and -1 are], answer = %s", myPass));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private static List<String> getBoardingPasses() {
+        return InputReader.readInput("/boarding-passes.txt").getInput();
+    }
+
+    static class BoardingPass {
+
+        int row;
+
+        int column;
+
+        int seatId;
+
+        public BoardingPass(String pass){
+            convert(pass);
+        }
+
+        void convert(String pass) {
+            row = Integer.parseInt(pass.substring(0, 7)
+                    .replaceAll("F", "0")
+                    .replaceAll("B", "1"), 2);
+            column = Integer.parseInt(pass.substring(7)
+                    .replaceAll("L", "0")
+                    .replaceAll("R", "1"), 2);
+            seatId = row * 8 + column;
+        }
+
+        @Override
+        public String toString() {
+            return "BoardingPass [" +
+                    "row=" + row +
+                    ", column=" + column +
+                    ", seatId=" + seatId +
+                    ']';
+        }
+    }
 }
